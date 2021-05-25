242e83d33f3bb1f7b767f564208e176065748c59
Jason Moore <jason@jasonmoore.xyz>
2021-05-25 23:47:46 +0100

    Fixed dmenu_custom appearing on the wrong monitor
    It's a little bit strange. Now it only appears on the incorrect monitor
    once before always being correct

--- config.h
+++ config.h
@@ -62,7 +62,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *dmenucustom[] = { "dmenu_custom", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucustom[] = { "dmenu_custom", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *dmenuemoji[] = { "emoji", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *backlightup[]  = { "xbacklight", "-display", "amdgpu_bl0", "-inc", "5", NULL };
